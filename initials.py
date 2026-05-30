5e5f26b9888b0f57440cfe043f927b6d93932d4a
Appiah-Kubi Bernard Ologo Dmerlin <2425400482@live.gctu.edu.gh>
2026-05-30 17:19:53 +0000

    Completed Assignment 1 - Initials

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
 
-def main():
-  pass #code goes here
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Appiah-Kubi Bernard Ologo Dmerlin
+# STUDENT ID: 2425400482
+# Email: 2425400482@live.gctu.edu.gh
+# Grader: Emma
+#
+# On my honor, Appiah-Kubi Bernard Ologo Dmerlin, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
+
+def main():
+    # Print my initials in large letters
+
+    print()
+    print("...ABO")
+    print()
+
+    print("...AAAAAAAAAAAA...BBBBBBBBBBBB...OOOOOOOOOOOO...")
+    print("...AA........AA...BB........BB...OO........OO...")
+    print("...AA........AA...BB........BB...OO........OO...")
+    print("...AA........AA...BB........BB...OO........OO...")
+    print("...AAAAAAAAAAAA...BBBBBBBBBBBB...OO........OO...")
+    print("...AA........AA...BB........BB...OO........OO...")
+    print("...AA........AA...BB........BB...OO........OO...")
+    print("...AA........AA...BB........BB...OO........OO...****")
+    print("...AA........AA...BB........BB...OO........OO...****")
+    print("...AA........AA...BBBBBBBBBBBB...OOOOOOOOOOOO...")
+
+    print()
 
 
 main()
